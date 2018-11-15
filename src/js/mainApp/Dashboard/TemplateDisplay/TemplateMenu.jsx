a055495447cff7b20e6c579cfc8f8b0c3afdbd9b
jatd <dhillon.jatinder@gmail.com>
2018-11-15 15:32:35 -0800

    Added a collapsible menu option when working in mobile mode

--- src/js/mainApp/Dashboard/TemplateDisplay/TemplateMenu.jsx
+++ src/js/mainApp/Dashboard/TemplateDisplay/TemplateMenu.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Input from "../../library/Input";
 import Button from "../../library/Button";
 import styles from "./styles.scss";
+import { cpus } from "os";
 
 function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -53,10 +54,22 @@ class TemplateMenu extends Component {
           valid: false,
           validation: value => validateText(value)
         }
-      }
+      },
+      collapsedMenu: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.collapseMenu = this.collapseMenu.bind(this);
+  }
+
+  componentDidMount() {
+    const windowWidth = document.getElementById("templateDisplay").offsetWidth;
+
+    if (windowWidth <= 768) {
+      this.setState({
+        collapsedMenu: true
+      });
+    }
   }
 
   handleChange(e) {
@@ -96,70 +109,90 @@ class TemplateMenu extends Component {
     }
   }
 
+  collapseMenu() {
+    this.setState({
+      collapsedMenu: !this.state.collapsedMenu
+    });
+  }
+
   render() {
     const { devMode } = this.props;
+    const { collapsedMenu } = this.state;
+
     return (
       <div className={styles.templateMenu}>
-        <div className={styles.templateMenuTitle}>
+        <div
+          className={styles.templateMenuTitleClickable}
+          onClick={this.collapseMenu}
+        >
           <i class="fas fa-align-justify" />
           Menu
         </div>
-        <div className={styles.renderHtml}>
-          <Button
-            onClick={() => {
-              window.getSelection().removeAllRanges();
-              return this.props.setDevMode();
-            }}
-            transparent
-            className={styles.renderHtml_button}
-          >
-            {devMode ? "Render HTML" : "Render Template"}
-          </Button>
-          {!devMode && (
+        {!collapsedMenu && (
+          <div className={styles.renderHtml}>
             <Button
-              onClick={() => selectText("templateDisplay")}
-              className={styles.renderHtml_selectButton}
+              onClick={() => {
+                window.getSelection().removeAllRanges();
+                return this.props.setDevMode();
+              }}
               transparent
+              className={styles.renderHtml_button}
             >
-              Copy to Clipboard
+              {devMode ? "Render HTML" : "Render Template"}
             </Button>
-          )}
-        </div>
-        <div className={styles.templateMenuTitle}>Send Email Template</div>
-        <form onSubmit={this.handleSubmit} className={styles.templateMenuForm}>
-          <Input
-            name="email"
-            type="text"
-            value={this.state.formControls.email.value}
-            placeholder="Email"
-            onChange={this.handleChange}
-          />
-          <Input
-            name="fromName"
-            type="text"
-            placeholder="From"
-            value={this.state.formControls.fromName.value}
-            onChange={this.handleChange}
-          />
-          <Input
-            name="subject"
-            type="text"
-            placeholder="Subject"
-            value={this.state.formControls.subject.value}
-            onChange={this.handleChange}
-          />
-          <div className={styles.buttonDiv}>
-            <Button
-              type="submit"
-              className={styles.submitButton}
-              transparent={true}
-            >
-              {" "}
-              Submit{" "}
-            </Button>
-            {this.props.sendingEmail && <div className={styles.hourglass} />}
+            {!devMode && (
+              <Button
+                onClick={() => selectText("templateDisplay")}
+                className={styles.renderHtml_selectButton}
+                transparent
+              >
+                Copy to Clipboard
+              </Button>
+            )}
           </div>
-        </form>
+        )}
+        {!collapsedMenu && (
+          <div className={styles.templateMenuTitle}>Send Email Template</div>
+        )}
+        {!collapsedMenu && (
+          <form
+            onSubmit={this.handleSubmit}
+            className={styles.templateMenuForm}
+          >
+            <Input
+              name="email"
+              type="text"
+              value={this.state.formControls.email.value}
+              placeholder="Email"
+              onChange={this.handleChange}
+            />
+            <Input
+              name="fromName"
+              type="text"
+              placeholder="From"
+              value={this.state.formControls.fromName.value}
+              onChange={this.handleChange}
+            />
+            <Input
+              name="subject"
+              type="text"
+              placeholder="Subject"
+              value={this.state.formControls.subject.value}
+              onChange={this.handleChange}
+            />
+            <div className={styles.buttonDiv}>
+              <Button
+                type="submit"
+                className={styles.submitButton}
+                transparent={true}
+              >
+                {" "}
+                Submit{" "}
+              </Button>
+              {this.props.sendingEmail && <div className={styles.hourglass} />}
+            </div>
+          </form>
+        )}
       </div>
     );
   }
