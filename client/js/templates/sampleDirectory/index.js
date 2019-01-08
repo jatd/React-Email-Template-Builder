b889a96939d43bf5cd32f9b93a76086f2d4beef1
jatd <dhillon.jatinder@gmail.com>
2019-01-08 15:39:38 -0800

    Removed old template and updated readme

--- client/js/templates/sampleDirectory/index.js
+++ client/js/templates/sampleDirectory/index.js
@@ -1,11 +1,5 @@
-import DentalReferral from "./DentalReferral";
 import VeteransNewsletter from "./VeteransNewsLetter";
 import YearEndNewsLetter from "./YearEndNewsLetter";
 import OnlineBookingEmailBlast from "./OnlineBookingEmailBlast";
 
-export {
-  DentalReferral,
-  VeteransNewsletter,
-  YearEndNewsLetter,
-  OnlineBookingEmailBlast
-};
+export { VeteransNewsletter, YearEndNewsLetter, OnlineBookingEmailBlast };
